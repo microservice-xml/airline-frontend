25bc7dc15cbed76871bff346c5cd3d47a2cd7b34
spiricnra2052019 <106819752+spiricnra2052019@users.noreply.github.com>
2023-03-14 18:51:12 +0100

    search bar html and css

--- app/src/pages/landing-page/index.tsx
+++ app/src/pages/landing-page/index.tsx
@@ -1,15 +1,19 @@
 import InfoCard from '../../components/InfoCard';
+import SearchComponent from '../../components/SearchComponent';
 import './index.scss'
 
 const LandingPage = () => {
 
 
-    return(<div className='landing'>
+    return (<div className='landing'>
+        <div className='landing__search'>
+            <SearchComponent></SearchComponent>
+        </div>
         <div className='landing__cards'>
             <div className='landing__cards-container'>
-                <InfoCard title='Cheap Flights' routerUrl='/news' imgUrl={require('../../assets/images/icons/takeoff1.png')}/>
-                <InfoCard title='Latest blogs' routerUrl='/news' imgUrl={require('../../assets/images/icons/blogs.png')}/>
-                <InfoCard title='Explore the world' routerUrl='/news' imgUrl={require('../../assets/images/icons/planet.png')}/>
+                <InfoCard title='Cheap Flights' routerUrl='/news' imgUrl={require('../../assets/images/icons/takeoff1.png')} />
+                <InfoCard title='Latest blogs' routerUrl='/news' imgUrl={require('../../assets/images/icons/blogs.png')} />
+                <InfoCard title='Explore the world' routerUrl='/news' imgUrl={require('../../assets/images/icons/planet.png')} />
             </div>
         </div>
     </div>)

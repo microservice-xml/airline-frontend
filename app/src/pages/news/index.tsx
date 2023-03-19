d154fc9ae2e9505d4432829e36c8d7528e260e94
Nikola Kalinic <93400532+NikolaKalinic@users.noreply.github.com>
2023-03-19 22:04:13 +0100

    Feature/register (#6)
    * finished register page with small problem about scss
    
    * small fix scss
    
    * added design for populars location
    
    * finished populars place
    
    * small fix

--- app/src/pages/news/index.tsx
+++ app/src/pages/news/index.tsx
@@ -2,43 +2,50 @@ import { useEffect, useState } from "react";
 import Article from "../../components/Article";
 import ArticleItem from "../../model/Article";
 import { getAllArticles } from "../../services/news/newsService";
-import './index.scss'
+import "./index.scss";
 
 function News() {
+  const [articles, setArticles] = useState<ArticleItem[]>([]);
 
-    const [articles, setArticles] = useState<ArticleItem[]>([])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        fetchData();
-    }, [])
+  const fetchData = async () => {
+    let response: any;
+    response = await getAllArticles();
+    setArticles(response.data as ArticleItem[]);
+  };
 
-    const fetchData = async () => {
-        let response : any;
-        response = await getAllArticles();
-        setArticles(response.data as ArticleItem[])
+  const renderArticles = () => {
+    let result = [];
+
+    if (articles.length === 0) {
+      return <div>There are no selected articles...</div>;
     }
 
-    const renderArticles = () => {
-
-        let result = []
-
-        if (articles.length === 0) {
-            return <div>There are no selected articles...</div>
-        }
-
-        for (let article of articles){
-            console.log(article)
-            result.push(<Article key={article.id} title={article.title} author={article.author} imgUrl={require('../../assets/images/backgrounds/' + article.slug + '.jpg')} slug={article.slug}/>)
-        }
-
-        return result;
+    for (let article of articles) {
+      result.push(
+        <Article
+          key={article.id}
+          title={article.title}
+          author={article.author}
+          imgUrl={require("../../assets/images/backgrounds/" +
+            article.slug +
+            ".jpg")}
+          slug={article.slug}
+        />
+      );
     }
 
-    return(<div className="news">
-        <div className="news__articles">
-            {renderArticles()}
-        </div>
-    </div>)
+    return result;
+  };
+
+  return (
+    <div className="news">
+      <div className="news__articles">{renderArticles()}</div>
+    </div>
+  );
 }
 
 export default News;
--- app/src/services/auth/authService.tsx
+++ app/src/services/auth/authService.tsx
@@ -1,7 +1,11 @@
-import { makeParametersList, request } from "./../base/HTTP";
+import { request } from "./../base/HTTP";
 import HttpMethod from "../../constants/HttpMethod";
 import LoginDto from "../../model/auth/loginDto";
 
 export async function login(loginDto: LoginDto) {
   return await request("/auth/authenticate", loginDto as any, HttpMethod.POST);
+}
+
+export async function registerUser(newUser: any) {
+  return await request("/auth/register", newUser as any, HttpMethod.POST);
 }
